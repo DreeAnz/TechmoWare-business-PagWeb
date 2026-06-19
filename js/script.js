981eaf42ce1c8d6d2452d8b47fa4005444fbafa9
Andre Anzures <andreanzures0@gmail.com>
2026-06-19 01:05:42 -0600

    techmoWare pagina web de mi negocio

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const contenedor = document.getElementById("burbujas-container");
+
+function crearBurbuja(){
+    const div = document.createElement("div");
+    div.classList.add("burbuja");
+    div.style.left = `${Math.random() * 100}%`;
+    const size = Math.random()*15+10;
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.animationName = "moverArriba";
+    div.style.animationDuration = `${Math.random() * 6+3}s`;
+    contenedor.appendChild(div);
+
+    div.addEventListener("animationend", () => {
+        div.remove();
+    });
+}
+
+window.addEventListener("DOMContentLoaded", () =>{
+    setInterval(crearBurbuja,300)
+});
